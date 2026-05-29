7309c6ac8e9bdf780a0e832dc6d24cb79df3aa9c
mahesh-09-12 <m8143177@gmail.com>
2026-05-29 18:36:09 +0530

    added db connection and prettier config

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Database is connected! ${connection.connection.host}`);
+  } catch (error) {
+    console.error("Database connection ERROR: ", error);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-const app = express()
+const app = express();
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hello, from Node server!</h2>")
-    res.status(200)
-})
+  res.send("<h2>Hello, from Node server!</h2>");
+  res.status(200);
+});
 
 app.listen(PORT, () => {
-    console.log(`PORT is listening at ${PORT}`);
-})
+  console.log(`PORT is listening at ${PORT}`);
+  connectDB();
+});
