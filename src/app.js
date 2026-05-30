0d32bb983205c5d740830ad26665bfba5e967827
mahesh-09-12 <m8143177@gmail.com>
2026-05-30 21:56:23 +0530

    run prettier

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 dotenv.config();
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
