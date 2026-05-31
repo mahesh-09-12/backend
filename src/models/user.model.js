cfb956e78ae8323906f5c79edafd52b6de2d43d6
mahesh-09-12 <m8143177@gmail.com>
2026-05-31 13:08:23 +0530

    ran prettier and configured multer and cloudinary

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,86 +1,89 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     usename: {
-        type: String,
-        required: [true, "Username is required"],
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     email: {
-        type: String,
-        unique: true,
-        trim: true,
-        required: [true, "Email address is required"],
-        lowercase: true
+      type: String,
+      unique: true,
+      trim: true,
+      required: [true, "Email address is required"],
+      lowercase: true,
     },
     fullName: {
-        type: String,
-        required: [true, "Fullname is required"],
-        trim: true,
-        index: true
+      type: String,
+      required: [true, "Fullname is required"],
+      trim: true,
+      index: true,
     },
     avatar: {
-        type: String,
-        required: [true, "Avatar is required"],
+      type: String,
+      required: [true, "Avatar is required"],
     },
     coverImage: {
-        type: String,
+      type: String,
     },
     watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Video"
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
     ],
     password: {
-        type: String,
-        required: [true, "Password is required"]
+      type: String,
+      required: [true, "Password is required"],
     },
     refreshToke: {
-        type: String
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-}, { timestamps: true })
+  );
+};
 
-UserSchema.pre("save", async function(next) {
-    if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
-    next()
-})
+UserSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
-UserSchema.methods.isPasswordCorrect = async function(password) {
-    return bcrypt.compare(password, this.password)
-}
-
-UserSchema.methods.generateAccessToken = function() {
-    return jwt.sign(
-        {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullName = this.fullName,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-        }
-    )
-}
-
-UserSchema.methods.generateRefreshToken = function() {
-    return jwt.sign(
-        {
-            _id: this._id
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
-
-export const User = mongoose.model("User", UserSchema)
+export const User = mongoose.model("User", UserSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,40 +1,43 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
-const VideoSchema = new Schema({
+const VideoSchema = new Schema(
+  {
     videoFile: {
-        type: String,
-        required: [true, "Video file is required"]
+      type: String,
+      required: [true, "Video file is required"],
     },
     thumbnail: {
-        type: String,
-        required: [true, "Thumbnail is required"]
+      type: String,
+      required: [true, "Thumbnail is required"],
     },
     title: {
-        type: String,
-        required: [true, "Title is required"]
+      type: String,
+      required: [true, "Title is required"],
     },
     description: {
-        type: String,
-        required: [true, "Description is required"]
+      type: String,
+      required: [true, "Description is required"],
     },
     duration: {
-        type: Number,
-        required: [true, "Duration is required"]
+      type: Number,
+      required: [true, "Duration is required"],
     },
     views: {
-        type: String,
-        default: 0
+      type: String,
+      default: 0,
     },
     isPublished: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
     owner: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-}, { timestamps: true })
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-mongoose.plugin(mongooseAggregatePaginate)
+mongoose.plugin(mongooseAggregatePaginate);
 
-export const Video = mongoose.model("Video", VideoSchema)
+export const Video = mongoose.model("Video", VideoSchema);
