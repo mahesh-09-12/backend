2a59eef297c6ffad0f3893e3263eee27dd62531a
mahesh-09-12 <m8143177@gmail.com>
2026-06-01 22:45:09 +0530

    setup complete

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Routes import
+import userRoute from "./routes/user.routes.js";
+
+// Declare routes
+app.use("/api/v1/users", userRoute);
+
 export { app };
