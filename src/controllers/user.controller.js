0e2f97ceec201208601a0723c2b1da706ce1e504
mahesh-09-12 <m8143177@gmail.com>
2026-06-03 23:47:24 +0530

    fixed register bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const userExisted = User.findOne({
+  const userExisted = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -32,7 +32,11 @@ const registerUser = asyncHandler(async (req, res) => {
 
   console.log(req.files);
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
