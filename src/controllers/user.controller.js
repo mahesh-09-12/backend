e5104e663d204023febf68f163e318224439b073
mahesh-09-12 <m8143177@gmail.com>
2026-06-06 00:19:42 +0530

    added access token refresh logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -174,4 +175,41 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
-export { registerUser, loginUser, logoutUser };
+const accessRefreshToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+  if(incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  const decodedToken = await jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+  const user = await User.findById(decodedToken?._id);
+  if(!user) {
+    throw new ApiError(401, "Invalid refresh token");
+  }
+
+  if(incomingRefreshToken !== user.refreshToken) {
+    throw new ApiError(401, "Refresh token is expired or used")
+  }
+
+  const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id);
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+
+  req
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", newRefreshToken, options)
+  .json(
+    new ApiResponse(
+      200,
+      {accessToken, refreshToken},
+      "Access token refreshed successfully"
+    )
+  )
+
+})
+export { registerUser, loginUser, logoutUser, accessRefreshToken };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  accessRefreshToken,
   loginUser,
   logoutUser,
   registerUser,
@@ -27,5 +28,6 @@ router.route("/login").post(loginUser);
 
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(accessRefreshToken)
 
 export default router;
