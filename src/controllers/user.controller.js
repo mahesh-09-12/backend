984ffaf904ef4bf7b9b5c21c6638b6eb5772bd9b
mahesh-09-12 <m8143177@gmail.com>
2026-06-05 23:53:11 +0530

    prevent encrypting password when saving user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,7 +10,7 @@ const generateAccessAndRefreshTokens = async (userId) => {
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
     user.refreshToken = refreshToken;
-    await user.save();
+    await user.save({ validateBeforeSave: false });
     return { accessToken, refreshToken };
   } catch (error) {
     throw new ApiError(
