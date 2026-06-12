df7499f3160e8048ef2895e1e2193c82176cd1d7
mahesh-09-12 <m8143177@gmail.com>
2026-06-12 22:59:27 +0530

    set up all routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
   accessRefreshToken,
+  changeAvatar,
+  changeCoverImage,
+  changePassword,
+  getChannelDetails,
+  getUserData,
+  getWatchHistory,
   loginUser,
   logoutUser,
   registerUser,
+  updateUserDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,20 @@ router.route("/login").post(loginUser);
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(accessRefreshToken);
+
+router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/change-details").patch(verifyJWT, updateUserDetails);
+
+router.route("/get-user-data").get(getUserData);
+
+router
+  .route("/change-avatar")
+  .patch(verifyJWT, upload.single("avatar"), changeAvatar);
+router
+  .route("/change-cover-image")
+  .post(verifyJWT, upload.single("coverImage"), changeCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getChannelDetails);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
