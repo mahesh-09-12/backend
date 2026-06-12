d5aba2ab4428e58aa63711b058f592a42952d2d2
mahesh-09-12 <m8143177@gmail.com>
2026-06-12 23:23:04 +0530

    add timestamps and run prettier

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,14 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchmea = new Schema({
-  subscriber: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
+const subscriptionSchmea = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  channel: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchmea);
